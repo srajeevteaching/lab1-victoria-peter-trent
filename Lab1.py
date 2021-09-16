92ea01997623a3753a77f037cf733fd74fd39d7f
peter <pjhope@loyola.edu>
2021-09-16 11:30:31 -0400

    Lab1 stuff

---
+++ Lab1.py
@@ -0,0 +1,12 @@
+#Programmers: Trent, Victoria, Peter
+#Course: CS151, Dr. Rajeev
+#Date: 9/16/21
+#Lab Num: 1
+#Program Inputs: number of mL
+#Program Outputs: number of teaspoons, number of tablespoons
+
+milliliters=input("number of milliliters: ")
+teaspoons=float(milliliters)/5
+tablespoons=float(teaspoons)/3
+print "Teaspoons: ", teaspoons
+print "Tablespoons: ", tablespoons
